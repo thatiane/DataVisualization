3cb1ef03bc3da6956d65b42ce615a1f4c1ae4278
amaumaury <amaurycombes@hotmail.fr>
2017-11-23 13:57:06 +0100

    Modify main in client app

--- Project_k/app/static/project.js
+++ Project_k/app/static/project.js
@@ -1,8 +1,10 @@
 const flask_server_url = 'http://0.0.0.0:5000';
-const url_name = 'https://www.cryptonator.com/api/currencies';
 const refreshPeriod = 300000;
 
-d3.json(url_name, function(error_name, data_name) {
-  currenciesFetcher = new CurrenciesFetcher(refreshPeriod);
+volumesFetcher = new VolumesFetcher(refreshPeriod);
 
-})
+promise = volumesFetcher.getVolumes();
+
+promise.then((data) => {
+  console.log(data);
+});
