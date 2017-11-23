1630908171e3def2184ee150924a1d45907a0bb8
amaumaury <amaurycombes@hotmail.fr>
2017-11-23 15:03:32 +0100

    Modify client main

--- Project_k/app/static/project.js
+++ Project_k/app/static/project.js
@@ -1,10 +1,14 @@
 const flask_server_url = 'http://0.0.0.0:5000';
 const refreshPeriod = 300000;
 
-volumesFetcher = new VolumesFetcher(refreshPeriod);
+dataFetcher = new DataFetcher(refreshPeriod);
 
-promise = volumesFetcher.getVolumes();
+promise = dataFetcher.getData();
 
 promise.then((data) => {
-  console.log(data);
+  currencies = data[0];
+  volumes = data[1];
+
+  console.log('Currencies: ' + currencies);
+  console.log('Volumes: ' + volumes);
 });
