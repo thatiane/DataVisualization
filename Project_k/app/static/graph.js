c783b1c45f29a45a41e57784591affbe738c9344
natalijagucevska <natalija.gucevska@epfl.ch>
2017-11-25 19:20:59 +0100

    Separate data parsing and show real data nodes

--- Project_k/app/static/graph.js
+++ Project_k/app/static/graph.js
@@ -1,77 +1,86 @@
+class Graph {
 
-function showGraph(data) {
+    constructor(currencies, exchanges) {
+        this.currencies = currencies;
+        this.exchanges = exchanges;
+    }
 
-    var yourDiv = document.getElementById("graph")
-    yourDiv.style.left = 0;
-    yourDiv.style.top = 0;
-    yourDiv.style.width = "100%";
-    yourDiv.style.height = "100%";
-    yourDiv.style.position = "absolute";
 
-    var cy = cytoscape({
-        container: yourDiv,
+    showGraph() {
+        var yourDiv = document.getElementById("graph")
+        yourDiv.style.width = "80%";
+        yourDiv.style.height = "90%";
+        yourDiv.style.left = "10%";
+        yourDiv.style.top = "15px";
+        yourDiv.style.position = "absolute";
+        yourDiv.style.border = "solid";
 
-        layout: {
-            name: 'cose',
-            padding: 10
-        },
 
-        style: cytoscape.stylesheet()
-            .selector('node')
-            .style({
-                'shape': 'data(faveShape)',
-                'content': 'data(name)',
-                'text-valign': 'center',
-                'text-outline-width': 1,
-                'text-outline-color': 'data(faveColor)',
-                'background-color': 'data(faveColor)',
-                'color': '#fff'
-            })
-            .selector(':selected')
-            .style({
-                'border-width': 2,
-                'border-color': '#525453'
-            })
-            .selector('edge')
-            .style({
-                'opacity': 0.666,
-             //   'width': 'mapData(strength, 70, 100, 2, 6)',
-                'width': 'data(strength)',
-                'target-arrow-shape': 'triangle',
-                'source-arrow-shape': 'circle',
-                'line-color': 'data(faveColor)',
-                'source-arrow-color': 'data(faveColor)',
-                'target-arrow-color': 'data(faveColor)'
-            })
-            .selector(':selected')
-            .style({
-                'border-width': 2,
-                'border-color': '#525453'
-            }),
 
-        elements: {
-            nodes: nodes([]),
+        this.cy = cytoscape({
+            container: yourDiv,
 
-            edges: [
-                {data: {source: 'j', target: 'e', faveColor: '#6FB1FC', strength: 10}},
-                {data: {source: 'j', target: 'k', faveColor: '#6FB1FC', strength: 4}},
-                {data: {source: 'j', target: 'g', faveColor: '#6FB1FC', strength: 1}},
-                {data: {source: 'e', target: 'k', faveColor: '#EDA1ED', strength: 1}},
-                {data: {source: 'k', target: 'g', faveColor: '#86B342', strength: 3}},
-            ]
+            layout: {
+                name: 'cose',
+                padding: 10
+            },
+
+            style: cytoscape.stylesheet()
+                .selector('node')
+                .style({
+                    'shape': 'ellipse',
+                    'content': 'data(id)',
+                    'text-valign': 'center',
+                    'text-outline-width': 1,
+                    'text-outline-color': '#afb1b0',
+                    'background-color': '#afb1b0',
+                    'color': '#fff'
+                })
+                .selector(':selected')
+                .style({
+                    'border-width': 2,
+                    'border-color': '#525453'
+                })
+                .selector('edge')
+                .style({
+                    'opacity': 0.666,
+                    'width': 'data(strength)',
+                    'line-color': 'data(faveColor)',
+                    'source-arrow-color': 'data(faveColor)',
+                    'target-arrow-color': 'data(faveColor)'
+                })
+                .selector(':selected')
+                .style({
+                    'border-width': 2,
+                    'border-color': '#525453'
+                }),
+
+            elements: {
+                nodes: this._setNodes(),
+               // edges: this._setEdges()
+            }
+        });
+    }
+
+    _setNodes() {
+        var result = []
+
+        for(let i=0; i<this.currencies.length; i++) {
+            result.push({data: {id: this.currencies[i]}});
         }
-    });
+
+        return result
+    }
+
+    _setEdges() {
+        var result = [
+                    {data: {source: 'j', target: 'e', faveColor: '#6FB1FC', strength: 10}},
+                    {data: {source: 'j', target: 'k', faveColor: '#6FB1FC', strength: 4}},
+                    {data: {source: 'j', target: 'g', faveColor: '#6FB1FC', strength: 1}},
+                    {data: {source: 'e', target: 'k', faveColor: '#EDA1ED', strength: 1}},
+                    {data: {source: 'k', target: 'g', faveColor: '#86B342', strength: 3}},
+                ]
+        return result
+    }
+
 }
-
-function nodes(data) {
-    //TODO : work with real data
-    var result = [
-                {data: {id: 'j', name: 'Jerry', weight: 20, faveColor: '#6FB1FC', faveShape: 'ellipse'}},
-                {data: {id: 'e', name: 'Elaine', weight: 20, faveColor: '#EDA1ED', faveShape: 'ellipse'}},
-                {data: {id: 'k', name: 'Kramer', weight: 20, faveColor: '#86B342', faveShape: 'ellipse'}},
-                {data: {id: 'g', name: 'George', weight: 20, faveColor: '#F5A45D', faveShape: 'ellipse'}}
-            ]
-    return result
-}
-
-
--- Project_k/app/static/project.js
+++ Project_k/app/static/project.js
@@ -5,39 +5,14 @@ dataFetcher = new DataFetcher(refreshPeriod);
 promise = dataFetcher.getData();
 
 promise.then((data) => {
-  let currencies = data[0];
-  let volumes = data[1];
-  let string_currency = '';
-  let start_string = false;
-  let list_currencies = [];
-  // transform the list of char in a list a String
-  for(let i = 0; i < currencies.length; i ++){
-      let char_ = currencies[i];
-      if(char_ == "'"){
-          if(start_string){
-              start_string = false;
-              string_currency += char_;
-              list_currencies.push(string_currency);
-              string_currency = '';
-          }else{
-              start_string = true;
-          }
-      }
-      if(start_string){
-          string_currency += char_;
-      }
-  }
-  // Now keep only the Names of the Currencies
-  let currencies_name = []
-  for(let i = 0; i < list_currencies.length; i ++){
-      let curr = list_currencies[i];
-      let split_curr = curr.split("/");
-      currencies_name.push(split_curr[2]);
-  }
-  //console.log("Name = " + currencies_name);
+    //Receive data
+    currencies = parseCurrencies(data[0]);
+    volumes = parseVolumes(data[1]);
 
-  ScorllBox(currencies_name)
+    //Show UI
+    ScorllBox(currencies);
 
-
-
+    //Create and show graph
+    const graph = new Graph(currencies, volumes);
+    graph.showGraph();
 });
