2d9d10b37c6d1574c8abd9525cb959d6d227cd04
Natalija <natalija.gucevska@epfl.ch>
2017-12-13 14:41:26 +0100

    Interactive heatmap

--- Project_k/app/static/heatmap.js
+++ Project_k/app/static/heatmap.js
@@ -9,66 +9,100 @@ class Heatmap {
     showHeatmap() {
 
         var xValues = []
+        var yValues = []
+
         var zValues = []
+        var coinInfo = [];
 
         var count = 0;
+        var visited = [];
+        var visited2 = [];
+
         for(var key1 in this.currencies) {
             xValues.push(key1);
+            yValues.push(key1);
             var values = [];
+            var cInfo = [];
+            visited.push(key1);
             for(var key2 in this.currencies) {
                 var pair = key1 + '/' + key2;
                 var edge = this.exchanges_total[pair];
-                if(typeof edge != "undefined") {
+                var v = NaN;
+                if(isDefined(edge)) {
                     count += 1;
-                    values.push(Math.log(parseFloat(edge["volume"])));
-                } else {
-                    values.push(0);
+                    v = (parseFloat(edge["volume"]));
                 }
+
+                if(visited.includes(key2)) {
+                    values.push(undefined);
+                } else {
+                    values.push(Math.log(v));
+                }
+
+                if(isNaN(v)) {
+                    v = 0;
+                }
+
+                var text = "Coin: " + this.currencies[key1]['name'] + "<br> Coin: "  + this.currencies[key2]['name']  + "<br> Volume: " + v;
+                cInfo.push(text);
             }
+            coinInfo.push(cInfo);
             zValues.push(values);
         }
 
-        console.log(count);
-
         var data = [{
           x: xValues,
-          y: xValues,
+          y: yValues,
           z: zValues,
+          mode: 'markers',
+          marker: {size:16},
+          text: coinInfo,
+          hoverinfo: 'text',
           type: 'heatmap',
           colorscale: [['0.0', 'rgb(255, 255, 255)'],
                        ['5.0', 'rgb(215,48,39)'],
-                       ['20.0', 'rgb(0, 0, 0)']]
-
+                       ['20.0', 'rgb(0, 0, 0)']],
+          showscale: false
         }];
 
         var layout = {
-          annotations: [],
           xaxis: {
             ticks: '',
+            showgrid: false,
             side: 'top',
-              autosize: false
+            autosize: false,
+            autorange: "reversed"
           },
           yaxis: {
-            ticks: '',
-            side: 'top',
-
-            autosize: false
+            autosize: false,
+              autorange: "reversed",
+              showgrid: false
           }
         };
-
 
         Plotly.newPlot('myDiv', data, layout);
 
         var myPlot = document.getElementById('myDiv');
 
-            myPlot.on('plotly_click', function(data){
+        myPlot.on('plotly_click', function(data){
             for(var i=0; i < data.points.length; i++){
                 //Do anything with clicked
+                 if(isDefined(data.points[i].z)) {
+                     var text = 'x = ' + data.points[i].x +
+                         'y = ' + data.points[i].y;
+                 }
+            }
+        });
 
-                var annotate_text = 'x = '+data.points[i].x +
-                              'y = '+data.points[i].y;
-
-                console.log(annotate_text);
+        var dragLayer = document.getElementsByClassName('nsewdrag')[0];
+        myPlot.on('plotly_hover', function(data){
+             for(var i=0; i < data.points.length; i++){
+                 if(isDefined(data.points[i].z)) {
+                     dragLayer.style.cursor = 'pointer'
+                 } else {
+                    dragLayer.style.cursor = 'default'
+                    Plotly.Fx.hover('myDiv',[ ]);
+                 }
             }
         });
     }
